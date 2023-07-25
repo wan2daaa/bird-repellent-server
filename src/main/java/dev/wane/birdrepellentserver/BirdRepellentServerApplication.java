5d62208b129393959ec9cbff2c10f543744e9466
wan2daaa <wan2daaa@gmail.com>
2023-07-25 11:32:31 +0900

    init project

---
+++ src/main/java/dev/wane/birdrepellentserver/BirdRepellentServerApplication.java
@@ -0,0 +1,13 @@
+package dev.wane.birdrepellentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdRepellentServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirdRepellentServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/wane/birdrepellentserver/BirdRepellentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wane.birdrepellentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdRepellentServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
